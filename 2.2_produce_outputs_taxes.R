9332bc930416e5d498fa7aee14b349d62d19465b
romainloup <romain.loup@unil.ch>
2024-09-05 17:24:26 +0200

    swiss taxes

---
+++ 2.2_produce_outputs_taxes.R
@@ -0,0 +1,32 @@
+#--------------------------------
+# Packages
+#--------------------------------
+library(plotly)
+
+
+taxes_communes = read.csv("/Users/rloup/Desktop/partial_association/IFD_classes_revenu_net_normaux.csv")
+View(taxes_communes)
+taxes_classes = read.csv("/Users/rloup/Desktop/partial_association/IFD_classes_revenu_normaux.csv")
+View(taxes_classes)
+
+taxes = taxes_communes[,4:12]/taxes_classes[,4:12]
+row.names(taxes) = taxes_communes$municipality
+taxes <- taxes %>% replace(is.na(.), 0)
+View(taxes)
+dim(taxes)
+
+Db = as.matrix(dist(taxes)^2)
+Kb = -0.5 * diag(sqrt(f)) %*% H %*% Db %*% t(H) %*% diag(sqrt(f)) # economical kernel
+
+dist_types = c("X","b")
+
+results_pol_taxes = RV(dist_types,f) # take a very long time with all distances
+results_pol_taxes$Z_RV
+
+# Select a distance to produce results
+mds_list = mds_fun(results_pol_taxes, f, dataVot, dist_types, "b") ; mds_list$mds_plot
+
+ggplotly(mds_list$mds_plot)
+
+dim(f)
+length(f)
